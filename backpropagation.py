eb463c4f79bcb9d71f4a9b0de25633f517b4590b
Raki Delmoro <rakidelmoro@gmail.com>
2024-08-22 03:23:21 +0000

    Brief implementation of backpropagation.

--- backpropagation.py
+++ backpropagation.py
@@ -11,19 +11,19 @@ def mlp_network(input_feature, hidden_feature, output_feature):
 
         return [input_to_hidden_weights, bias_hidden], [hidden_to_output_weights, bias_output]
 
-    sigmoid_not_derivative = lambda input_x: 1 / (1 + np.exp(-input_x))
-    sigmoid_derivative = lambda input_x: input_x * (1 - input_x)
+    sigmoid_activation_not_derivate = lambda input_x: 1 / (1 + np.exp(-input_x))
+    sigmoid_activation_derivative = lambda input_x: input_x * (1 - input_x)
 
     def forward_in_network(input_data):
         input_to_hidden_weight_and_bias, hidden_to_output_weight_and_bias = weight_and_bias_initialization()
 
         # Input to Hidden layer
         hidden_activation = np.dot(input_data, input_to_hidden_weight_and_bias[0]) + input_to_hidden_weight_and_bias[1]
-        hidden_output_nodes = sigmoid_not_derivative(hidden_activation)
+        hidden_output_nodes = sigmoid_activation_not_derivate(hidden_activation)
 
         # Hidden to output layer
         output_activation = np.dot(hidden_output_nodes, hidden_to_output_weight_and_bias[0]) + hidden_to_output_weight_and_bias[1]
-        network_prediction = sigmoid_not_derivative(output_activation)
+        network_prediction = sigmoid_activation_not_derivate(output_activation)
 
         return network_prediction, hidden_output_nodes
 
@@ -31,18 +31,18 @@ def mlp_network(input_feature, hidden_feature, output_feature):
         input_to_hidden_weight_and_bias, hidden_to_output_weight_and_bias = weight_and_bias_initialization()
 
         # Compute the output layer error
-        network_last_layer_delta = network_error * sigmoid_derivative(network_prediction)
+        network_last_layer_delta = network_error * sigmoid_activation_derivative(network_prediction)
 
         # Compute the hidden layer error
         network_hidden_layer_error = np.dot(network_last_layer_delta, hidden_to_output_weight_and_bias[0].T)
-        network_hidden_layer_delta = network_hidden_layer_error * sigmoid_derivative(hidden_output_nodes)
+        network_hidden_layer_delta = network_hidden_layer_error * sigmoid_activation_derivative(hidden_output_nodes)
 
-        # Hidden to output weight update
+        # output to hidden weight update
         hidden_to_output_weight_and_bias[0] += np.dot(hidden_output_nodes.T, network_last_layer_delta) * learning_rate
         # output bias update
         hidden_to_output_weight_and_bias[1] += np.sum(network_last_layer_delta, axis=0, keepdims=True) * learning_rate
 
-        # input to hidden weight update
+        # hidden to input weight update
         input_to_hidden_weight_and_bias[0] += np.dot(input_data.T, network_hidden_layer_delta) * learning_rate
         # hidden bias update 
         input_to_hidden_weight_and_bias[1] += np.sum(network_hidden_layer_delta, axis=0, keepdims=True) * learning_rate
