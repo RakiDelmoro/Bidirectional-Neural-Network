9db1cd7f17c1fee8d92cb5e030d0de1db33a9c0e
Raki Delmoro <rakidelmoro@gmail.com>
2024-09-09 13:14:46 +0000

    Comment out unused codes for training test.

--- toy_problem.py
+++ toy_problem.py
@@ -1,10 +1,9 @@
-import random
 import torch
+import random
 
 def generate_input_and_expected_pair(number_will_generated):
     # [[w1, w2], [w3, w4]]
-    # target_weights = torch.tensor([[-9/8, 5/8], [7/8, -3/8]], dtype=torch.float32, device="cuda")
-    target_weights = torch.tensor([[3, 5], [7, 9]], dtype=torch.float32, device="cuda")
+    target_weights = torch.tensor([[3, 5], [7, 9]], dtype=torch.float32, device="cuda").inverse()
 
     generated_pair_of_number = []
     for _ in range(number_will_generated):
@@ -15,29 +14,27 @@ def generate_input_and_expected_pair(number_will_generated):
     return generated_pair_of_number
 
 def neural_network(learning_rate=0.001):
-    # [[w1, w2], [w3, w4]]
+    # Define parameters - [[w1, w2], [w3, w4]]
     l2r_weights = torch.tensor([[0.1233, 0.5365], [0.1457, 0.1371]], dtype=torch.float32, device="cuda")
-    r2l_weights = torch.tensor([[0.2334, 0.5345], [0.6456, 0.6588]], dtype=torch.float32, device="cuda")
-    # r2l_weights = l2r_weights
 
     counter = 0
-
     while True:
-        # Training AI MODEL
+        # Use the inverse value of l2r weights for right to left weights
+        r2l_weights = l2r_weights.inverse()
+
+        # Prepare Input data
         data = generate_input_and_expected_pair(1)
         l2r_input_data = data[0][0]
         r2l_input_data = data[0][1]
 
-        print(f"target for left to right: {r2l_input_data}")
-        print(f"target for right to left: {l2r_input_data}")
         # forward pass
         l2r_output_nodes = torch.matmul(l2r_input_data, l2r_weights)
         # backward pass
-        r2l_output_nodes = torch.matmul(l2r_output_nodes, r2l_weights)
+        r2l_output_nodes = torch.matmul(r2l_input_data, r2l_weights)
         # Left to right nodes loss
         l2r_output_nodes_loss = 2 * (l2r_output_nodes - r2l_input_data)
         # Right to left nodes loss
-        r2l_output_nodes_loss = 2 * (r2l_output_nodes - l2r_input_data)
+        # r2l_output_nodes_loss = 2 * (r2l_output_nodes - l2r_input_data)
 
         # If encountered nan value immediately stop training for debugging purposes
         if torch.isnan(r2l_output_nodes).any().item():
@@ -50,10 +47,10 @@ def neural_network(learning_rate=0.001):
         l2r_weight_3_gradient = l2r_input_data[0, 1] * l2r_output_nodes_loss[0, 0]
         l2r_weight_4_gradient = l2r_input_data[0, 1] * l2r_output_nodes_loss[0, 1]
         # Right to left calculate local gradient
-        r2l_weight_1_gradient = r2l_input_data[0, 0] * r2l_output_nodes_loss[0, 0]
-        r2l_weight_2_gradient = r2l_input_data[0, 0] * r2l_output_nodes_loss[0, 1]
-        r2l_weight_3_gradient = r2l_input_data[0, 1] * r2l_output_nodes_loss[0, 0]
-        r2l_weight_4_gradient = r2l_input_data[0, 1] * r2l_output_nodes_loss[0, 1]
+        # r2l_weight_1_gradient = r2l_input_data[0, 0] * r2l_output_nodes_loss[0, 0]
+        # r2l_weight_2_gradient = r2l_input_data[0, 0] * r2l_output_nodes_loss[0, 1]
+        # r2l_weight_3_gradient = r2l_input_data[0, 1] * r2l_output_nodes_loss[0, 0]
+        # r2l_weight_4_gradient = r2l_input_data[0, 1] * r2l_output_nodes_loss[0, 1]
 
         # Left to right calculate update parameters
         l2r_weight_1 = l2r_weights[0, 0] - learning_rate * l2r_weight_1_gradient
@@ -61,10 +58,10 @@ def neural_network(learning_rate=0.001):
         l2r_weight_3 = l2r_weights[1, 0] - learning_rate * l2r_weight_3_gradient
         l2r_weight_4 = l2r_weights[1, 1] - learning_rate * l2r_weight_4_gradient
         # Right to left calculate update parameters
-        r2l_weight_1 = r2l_weights[0, 0] - learning_rate * r2l_weight_1_gradient
-        r2l_weight_2 = r2l_weights[0, 1] - learning_rate * r2l_weight_2_gradient
-        r2l_weight_3 = r2l_weights[1, 0] - learning_rate * r2l_weight_3_gradient
-        r2l_weight_4 = r2l_weights[1, 1] - learning_rate * r2l_weight_4_gradient
+        # r2l_weight_1 = r2l_weights[0, 0] - learning_rate * r2l_weight_1_gradient
+        # r2l_weight_2 = r2l_weights[0, 1] - learning_rate * r2l_weight_2_gradient
+        # r2l_weight_3 = r2l_weights[1, 0] - learning_rate * r2l_weight_3_gradient
+        # r2l_weight_4 = r2l_weights[1, 1] - learning_rate * r2l_weight_4_gradient
 
         # Assign the new updated weights
         l2r_weights[0, 0] = l2r_weight_1
@@ -72,15 +69,18 @@ def neural_network(learning_rate=0.001):
         l2r_weights[1, 0] = l2r_weight_3
         l2r_weights[1, 1] = l2r_weight_4
 
-        r2l_weights[0, 0] = r2l_weight_1
-        r2l_weights[0, 1] = r2l_weight_2
-        r2l_weights[1, 0] = r2l_weight_3
-        r2l_weights[1, 1] = r2l_weight_4
+        # r2l_weights[0, 0] = r2l_weight_1
+        # r2l_weights[0, 1] = r2l_weight_2
+        # r2l_weights[1, 0] = r2l_weight_3
+        # r2l_weights[1, 1] = r2l_weight_4
 
+        print(f"target for left to right: {r2l_input_data}")
+        print(f"target for right to left: {l2r_input_data}")
         print(f"Left to right output node: {l2r_output_nodes}")
         print(f"Right to left output node: {r2l_output_nodes}")
         print(f"Left to right weights: [[{l2r_weight_1}, {l2r_weight_2}], [{l2r_weight_3}, {l2r_weight_4}]]")
-        print(f"Right to left weights: [[{r2l_weight_1}, {r2l_weight_2}], [{r2l_weight_3}, {r2l_weight_4}]]")
+        # print(f"Right to left weights: [[{r2l_weight_1}, {r2l_weight_2}], [{r2l_weight_3}, {r2l_weight_4}]]")
+        print(f"Right to left weights: [[{r2l_weights[0, 0]}, {r2l_weights[0, 1]}], [{r2l_weights[1, 0]}, {r2l_weights[1, 1]}]]")
         counter += 1
 
 neural_network()
